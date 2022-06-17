03231583528ab194b886cd2d6337d72b2e726a6c
timoheutsrs <timo.heuts@retailsolutions.nl>
2022-06-17 16:56:05 +0200

    assignment1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col cols="3"></v-col>
+          <v-col cols="6">
+            <v-card
+                cols="6"
+                elevation="12"
+                class="pa-3"
+            >
+
+              <v-text-field v-model="firstname"
+                label="First name"></v-text-field>
+              <v-text-field
+                  label="Last name"
+              v-model="lastname"></v-text-field>
+              <v-btn
+                  color="primary"
+                  elevation="2"
+                  v-on:click="clear"
+              >
+                Clear
+              </v-btn>
+
+              <v-card class="mt-3 pa-3">
+                {{firstname}} {{lastname}}
+              </v-card>
+
+            </v-card>
+
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+  },
+
+  data: () => ({
+    firstname:'',
+    lastname: '',
+
+    //
+  }),
+  methods: {
+    clear: function () {
+      this.firstname = '';
+      this.lastname='';
+    }
+  }
+};
+</script>
